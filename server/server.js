bfdeb15201953414d11703451fb669947b56e400
Steven Yee <steveny@Stevens-MacBook-Pro.local>
2017-01-15 22:33:14 -0800

    added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -58,6 +59,30 @@ app.delete('/todos/:id', (req, res) => {
         }
     }).catch((e) => res.status(404).send());
 });
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send('not a valid Id');
+    }
+    
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    
+    Todo.findByIdAndUpdate(id, {$set: body}, {new:true}).then((todo) => {
+        if(!todo){
+            return res.status(404);
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}.`);
